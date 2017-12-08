1cae4eed8075bac57755465b0c7c0dd3a13233f7
Juho Perälä <juperala@dnainternet.net>
2017-12-08 11:04:56 +0100

    Removed obsolete site content.

--- site/test2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://c164d3ul86.execute-api.eu-west-1.amazonaws.com/prod/parking-get-usage"
-    }).then(function(data) {
-       $('.greeting-id').append(data.statusCode);
-       $('.greeting-content').append(data.body);
-    });
-});
