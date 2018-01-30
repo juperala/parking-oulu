b7eeb81ec9822a937aa85d3e9d20b643d36aec2c
Juho Perälä <juperala@dnainternet.net>
2018-01-30 12:02:39 +0100

    Updated lambda to support paginated scan method.

--- oulu-parking-station-details/index.js
+++ oulu-parking-station-details/index.js
@@ -9,22 +9,40 @@ const tableName = process.env.tableName;
  * Lambda function handler.
  */
 exports.handler = (event, context, callback) => {
-  const done = (err, res) =>
+  var params = null;
+  var result = [];
+
+  const done = (err, data) => {
     callback(null, {
       statusCode: err ? "400" : "200",
-      body: err ? err.message : JSON.stringify(res["Items"]),
+      body: err ? err.message : JSON.stringify(data),
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*"
       }
     });
+  }
+
+  const onScan = (err, res) => {
+    if (err) {
+      done(err, result);
+    } else {
+      result.push(...(res["Items"]));
+      if (typeof res.LastEvaluatedKey != 'undefined') {
+        params.ExclusiveStartKey = res.LastEvaluatedKey;
+        dynamo.scan(params, onScan);
+      } else {
+        done(err, result);
+      }
+    }
+  }
 
   switch (event.httpMethod) {
     case "GET":
       const stationId = event.queryStringParameters.ParkingStationId;
       const start = event.queryStringParameters.from;
       const end = event.queryStringParameters.to;
-      var params = {
+      params = {
         FilterExpression:
           "(ParkingStationId = :id) AND (#timestamp between :start and :stop)",
         ExpressionAttributeValues: {
@@ -38,7 +56,7 @@ exports.handler = (event, context, callback) => {
         ReturnConsumedCapacity: "TOTAL",
         TableName: tableName
       };
-      dynamo.scan(params, done);
+      dynamo.scan(params, onScan);
       break;
     default:
       done(new Error(`Unsupported method "${event.httpMethod}"`));
