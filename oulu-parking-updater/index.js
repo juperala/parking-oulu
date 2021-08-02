82c2da55adbaf85fd1322e1c532d560baca71fe0
Juho Perälä <juperala@dnainternet.net>
2021-08-02 10:41:04 +0300

    Update for graphql

--- oulu-parking-updater/index.js
+++ oulu-parking-updater/index.js
@@ -1,11 +1,9 @@
 "use strict";
 
-const stationsUrl =
-  "https://www.oulunliikenne.fi/public_traffic_api/parking/parkingstations.php";
-const stationUrl =
-  "https://www.oulunliikenne.fi/public_traffic_api/parking/parking_details.php?parkingid=";
 
-const request = require("request");
+const apiUrl = "https://api.oulunliikenne.fi/proxy/graphql";
+
+const { request } = require("graphql-request");
 const doc = require("dynamodb-doc");
 const dynamo = new doc.DynamoDB();
 
@@ -16,52 +14,51 @@ const stationStatusTableName = process.env.stationStatusTableName;
  * Lambda function handler.
  */
 exports.handler = (event, context, callback) => {
-  request(
-    {
-      url: stationsUrl,
-      json: true
-    },
-    function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        body.parkingstation.forEach(element => {
-          request(
-            {
-              url: stationUrl + element.id,
-              json: true
-            },
-            function(error, response, body) {
-              if (!error && response.statusCode === 200) {
-                let info = {
-                  ParkingStationId: Number(element.id),
-                  Name: element.name,
-                  Address: body.address,
-                  Coordinates: JSON.parse(element.geom).coordinates
-                };
-                info = Object.assign(
-                  info,
-                  body.freespace && { Freespace: Number(body.freespace) },
-                  body.totalspace && { Totalspace: Number(body.totalspace) }
-                );
-                putItemToTable(info, stationsTableName);
 
-                let status = {
-                  ParkingStationId: Number(element.id),
-                  Timestamp: formatDateToISO(body.timestamp)
-                };
-                status = Object.assign(
-                  status,
-                  body.freespace && { Freespace: Number(body.freespace) },
-                  body.totalspace && { Totalspace: Number(body.totalspace) }
-                );
-
-                putItemToTable(status, stationStatusTableName);
-              }
-            }
-          );
-        });
-      }
+  const query = `{
+    carParks {
+      carParkId
+      name
+      lat
+      lon
+      maxCapacity
+      spacesAvailable
     }
-  );
+  }`
+
+  request(apiUrl, query).then(data => {
+    console.log(data);
+
+    data.carParks.forEach(element => {
+
+    // [ { "N" : "25.469227" }, { "N" : "65.007913" }]
+    let info = {
+        ParkingStationId: Number(element.carParkId),
+        Name: element.name,
+        Address: element.name,
+        Coordinates:  [element.lon, element.lat]
+      };
+      info = Object.assign(
+        info,
+        element.spacesAvailable && { Freespace: Number(element.spacesAvailable) },
+        element.maxCapacity && { Totalspace: Number(element.maxCapacity) }
+      );
+      putItemToTable(info, stationsTableName);
+
+      let status = {
+        ParkingStationId: Number(element.carParkId),
+        Timestamp: new Date().toISOString()
+      };
+      status = Object.assign(
+        status,
+        element.spacesAvailable && { Freespace: Number(element.spacesAvailable) },
+        element.maxCapacity && { Totalspace: Number(element.maxCapacity) }
+      );
+
+      putItemToTable(status, stationStatusTableName);      
+
+    })
+  });
 };
 
 function putItemToTable(item, table) {
